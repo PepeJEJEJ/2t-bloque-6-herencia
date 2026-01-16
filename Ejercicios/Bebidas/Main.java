2c0de179bf0fefe9e80cc2c1a82ac01f8ca50be6
PepeJEJEJ <jah0005@alu.medac.es>
2026-01-16 18:40:28 +0100

    Add Bebidas Main class and update CocheAvanz App
    Added a new Main.java for the Bebidas exercise with initial class structure and comments. Updated App.java in CocheAvanz to use Vehiculo instances instead of Coche, and added a second Vehiculo object.

---
+++ Ejercicios/Bebidas/Main.java
@@ -0,0 +1,12 @@
+package Ejercicios.Bebidas;
+
+/*Ejercicio almacén de bebidas.
+Supongamos que debemos hacer un software para un almacén de bebidas.
+En el almacén hay zumos, refrescos y bebidas alcohólicas. Todas tienen nombre, marca y precio (precio por cada litro). Añade también a cada una algún dato extra que consideres importante de cada bebida en concreto.
+Todas las clases deben tener los siguientes métodos:
+- Un método que reciba la edad del usuario e indique si puede tomar dicha bebida o no.
+- Un método que reciba el volumen de la botella e indique el precio total.*/
+
+public class Main {
+    
+}
--- Ejercicios/CocheAvanz/App.java
+++ Ejercicios/CocheAvanz/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception{
-        Coche coche1=new Coche("Gei", null, 0, 0, 0, false);
+        Vehiculo vehiculo1=new Vehiculo("Gei", null, 0, 0, 0, false);
+        Vehiculo vehiculo2=new Vehiculo("Gei", null, 0, 0, 0, false);
     }
 }
