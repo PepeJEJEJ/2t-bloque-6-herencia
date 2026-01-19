b89fa67a8bec0063863d48a46c081b5c8633defd
PepeJEJEJ <jah0005@alu.medac.es>
2026-01-19 18:05:29 +0100

    Create Main.java

---
+++ Ejercicios/Codigos_Y_Mazmorras/Main.java
@@ -0,0 +1,20 @@
+package Codigos_Y_Mazmorras;
+
+/*
+ * Ejercicio 4. Personajes de un juego.
+ * Todos los personajes tienen un id, un nombre, una vida y un daño. En nuestro
+ * juego hay personajes que controla el jugador, y enemigos que maneja la
+ * máquina.
+ * Los jugadores también tienen fuerza (se mide en 0-100).
+ * Los enemigos también tienen poderMagico (0-1 con decimales).
+ * Crea 2 jugadores y 3 enemigos.
+ * //Ampliación implementa la clase Juego que tenga un método batalla() que
+ * permita pasarle por parámetro un jugador y un enemigo, y nos indique quién
+ * gana la batalla (gana quien tenga más fuerza/podermagico*100).
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+}
