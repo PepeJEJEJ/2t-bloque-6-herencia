643ae285b9519b57bf528a6c5b126f4bb5fe3881
PepeJEJEJ <jah0005@alu.medac.es>
2026-01-23 11:26:38 +0100

    Update Main.java

--- Ejercicios/Web/Main.java
+++ Ejercicios/Web/Main.java
@@ -4,4 +4,6 @@ public class Main{
 }
 
 // SOBREECARGA ES MODIFICAR UN METODO DEL PADRE EN EL HIJO (EL OVERRIDE)
-// POLIMORFISMO ES
+// POLIMORFISMO = MISMA LLAMADA, RESULTADO DISTINTO SEGÚN QUIÉN LO EJECUTE
+
+/*Hay qe hacer que una aplcifacio de dibujo necesita crear figs geomertrigas, cuadrado cirdcculo etc Todas tieenn color, grosor del borde , id u sibmgrado si no ademas de sus medidas correspondientes (lado radio eftc)  dofefrentes cada una*/
