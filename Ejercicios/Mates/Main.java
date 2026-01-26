f532982abad959b3ed6c729514755d4fa98ae59a
PepeJEJEJ <jah0005@alu.medac.es>
2026-01-26 13:04:24 +0100

    Add basic math utility and main test class
    Introduces the Matematicas class with basic arithmetic operations (sum, subtract, multiply, divide, power) and a Main class to demonstrate usage.

---
+++ Ejercicios/Mates/Main.java
@@ -0,0 +1,8 @@
+package Mates;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Matematicas.sumar(4, 6));
+        System.out.println(Matematicas.potencia(2, 5));
+    }
+}
