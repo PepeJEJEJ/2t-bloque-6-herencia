0e9f126039c98ff7864136fd8fff47eae8f46160
PepeJEJEJ <jah0005@alu.medac.es>
2026-03-27 10:35:22 +0100

    Update Criptos.java

--- Examen2T/Criptos.java
+++ Examen2T/Criptos.java
@@ -1,17 +1,15 @@
-public class Criptos extends Activos{
+public class Criptos extends Activos {
     private boolean Blockchain;
-    public Criptos(boolean Blockchain) {
+
+    public Criptos(String Nombre, Double Cantidad, boolean Blockchain) {
         super(Nombre, Cantidad);
-        this.Blockchain=Blockchain;
-    }
-    public boolean getBlockchain() {
-        return Blockchain;
-    }
-    public void setBlockchain(boolean Blockchain) {
         this.Blockchain = Blockchain;
     }
+
     @Override
-    public String tostring(){
-        return "Nombre "+Nombre+" Cantidad: "+Cantidad+" Es blockchain "+Blockchain;
+    public String toString() {
+        return Nombre + " (" + Cantidad + ") Blockchain: " + Blockchain;
     }
+}
+
 }
